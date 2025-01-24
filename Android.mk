c13bf85e207dcd2b923af9d04098b4c483226840
icdada <2045440378@qq.com>
2025-01-24 15:48:11 +0800

    up

--- Android.mk
+++ Android.mk
@@ -6,12 +6,7 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-# Define the lunch choices
-COMMON_LUNCH_CHOICES += \
-    omni_PD2148-eng \
-    omni_PD2148-user \
-    omni_PD2148-userdebug
-	
+
 ifeq ($(TARGET_DEVICE),PD2148)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,10 +5,12 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_PD2148.mk
-
-COMMON_LUNCH_CHOICES := \
+# Define the lunch choices
+COMMON_LUNCH_CHOICES += \
     omni_PD2148-user \
     omni_PD2148-userdebug \
     omni_PD2148-eng
+
+# Define the product makefiles
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_PD2148.mk
